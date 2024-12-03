21a361b7f33233c267e9d5977552d2ca91da761e
RDR <rsderidd@yahoo.com>
2024-12-02 22:30:20 -0500

    model first draft

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,10 +7,47 @@ const schema = a
       .model({
         email: a.string(),
         profileOwner: a.string(),
+        test: a.string()
       })
       .authorization((allow) => [
         allow.ownerDefinedIn("profileOwner"),
       ]),
+    Accounts: a
+      .model({
+        name: a.string(),
+        type: a.string(),
+        birthdate: a.string(),
+        min_withdrawal_date: a.string(),
+        starting_balance: a.float()
+      })
+      .authorization((allow) => [
+        allow.ownerDefinedIn("profileOwner"),
+      ]),
+    Holdings: a
+      .model({
+        account_id: a.string(),
+        name: a.string(),
+        purchase_date: a.string(),
+        amount_paid: a.float(),
+        maturity_date: a.string(),
+        rate: a.float(),
+        amount_at_maturity: a.float()
+      })
+      .authorization((allow) => [
+        allow.ownerDefinedIn("profileOwner"),
+      ]),
+    Transactions: a
+      .model({
+        account_id: a.string(),
+        type: a.string(),
+        xtn_date: a.string(),
+        amount: a.float(),
+        holding_id: a.string()
+      })
+      .authorization((allow) => [
+        allow.ownerDefinedIn("profileOwner"),
+      ]),
+        
   })
   .authorization((allow) => [allow.resource(postConfirmation)]);
 export type Schema = ClientSchema<typeof schema>;
